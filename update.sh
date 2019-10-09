f615738d738e93aff8dcdf773c56ea8f07bc4480
MRLINKERRORSYSTEM <54674143+Hackermrlinkerrorsystem@users.noreply.github.com>
2019-10-10 04:43:56 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,50 @@
+#Greet's To
+#THCA             
+#Tool For Hacking 
+#Authors : MRLinkerrorsystem
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/wifisky/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/404system
+
+clear
+
+mkdir 404system
+
+clear
+
+cd 404system
+
+clear
+
+git clone https://github.com/404system/wifisky/.git
+
+clear
+
+cd wifisky
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
